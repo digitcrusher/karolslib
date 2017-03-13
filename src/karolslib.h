fa3da77f5feac7d227c10008a5b67a4ca9019e10
Karol Łacina <karoleklacina@gmail.com>
2017-03-13 22:05:26 +0100

    fixed

--- src/karolslib.h
+++ src/karolslib.h
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifdef _KAROLSLIB_H_
+#ifndef _KAROLSLIB_H_
 #define _KAROLSLIB_H_
 
 void karolslib_init();
